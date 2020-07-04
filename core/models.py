2a1346e498ccdb6aa44da9c1b3655b01a20d7ef0
unknown <jbj1996@gmail.com>
2020-07-04 13:08:49 -0400

    added classes for status and severity in core.models:internal dictionaries have placeholder strings in second half of tuple

--- core/models.py
+++ core/models.py
@@ -7,7 +7,36 @@ class Contributor(AbstractBaseUser):
     USERNAME_FIELD = "username"
     email = models.EmailField(max_length=300)
     bio = models.CharField(max_length=1000)
-    
+
+class Status(models.Model):
+    Not_Started = '0'
+    In_Progress = '1'
+    Completed = '2'
+    Status_Choices = [
+        (Not_Started, 'Not Started'),
+        (In_Progress, 'In Progress'),
+        (Completed, 'Completed'),
+    ]
+    status_choices = models.CharField(
+        max_length=1,
+        choices=Status_Choices,
+        default=Not_Started
+    )
+class Severity(models.Model):
+    Low = '0'
+    Medium = '1'
+    High = '2'
+    Severity_Choices = [
+        (Low, 'Low'),
+        (Medium, 'Medium'),
+        (High, 'High'),
+    ]
+    Severity_choices = models.CharField(
+        max_length=1,
+        choices=Severity_Choices,
+        default=Medium
+    )
+
 
 class Issue(models.Model):
     title = models.CharField(max_length=100)
