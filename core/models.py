25d884a65bc936e1ee25553743879f1e82dd85d3
unknown <jbj1996@gmail.com>
2020-07-05 22:52:30 -0400

    Added models for Comment and Group, removed primary key where superfluous, committed status and severity

--- core/models.py
+++ core/models.py
@@ -9,41 +9,52 @@ class Contributor(AbstractBaseUser):
     bio = models.CharField(max_length=1000)
 
 class Status(models.Model):
-    Not_Started = '0'
-    In_Progress = '1'
-    Completed = '2'
+    Not_Started = 0
+    In_Progress = 1
+    Completed = 2
     Status_Choices = [
         (Not_Started, 'Not Started'),
         (In_Progress, 'In Progress'),
         (Completed, 'Completed'),
     ]
-    status_choices = models.CharField(
-        max_length=1,
+    status_choices = models.IntegerField(
         choices=Status_Choices,
         default=Not_Started
     )
 class Severity(models.Model):
-    Low = '0'
-    Medium = '1'
-    High = '2'
+    Low = 0
+    Medium = 1
+    High = 2
     Severity_Choices = [
         (Low, 'Low'),
         (Medium, 'Medium'),
         (High, 'High'),
     ]
-    Severity_choices = models.CharField(
-        max_length=1,
+    Severity_choices = models.IntegerField(
         choices=Severity_Choices,
         default=Medium
     )
-
+class Group(models.Model):
+    name = models.CharField(max_length=150)
 
 class Issue(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=10000)
+    description = models.CharField(max_length=2048)
     author = models.OneToOneField(
         Contributor,
-        on_delete=models.CASCADE,
-        primary_key=True,
+        on_delete=models.SET_NULL,
+        null=True
     )
+
+class Comment(models.Model):
+    body = models.CharField(max_length=2048)
+    author = models.OneToOneField(
+        Contributor,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    issue = models.OneToOneField(
+        Issue,
+        on_delete=models.CASCADE,
+    ) 
 
