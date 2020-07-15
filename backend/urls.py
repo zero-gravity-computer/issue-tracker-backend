2a1762735619482ed3c4f19f00bde5eb75d4f18d
unknown <jbj1996@gmail.com>
2020-07-15 09:50:55 -0400

    added base URL's for json serializer and abstracted a function to replace individual model serializers

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,17 @@ from django.urls import path
 from core import views, models
 
 urlpatterns = [
-    path(r'issues/', views.read_issues),
-    path(r'issues/<id>', views.read_issue),
+    path(r'issues.json/', views.read_many(models.Issue)),
+    path(r'issues/<id>.json/', views.read_one(models.Issue)),
+    path(r'contributors.json/', views.read_many(models.Contributor)),
+    path(r'contributors/<id>.json/', views.read_one(models.Contributor)),
+    path(r'organizations.json/', views.read_many(models.Organization)),
+    path(r'organizations/<id>.json/', views.read_one(models.Organization)),
+    path(r'teams.json/', views.read_many(models.Team)),
+    path(r'teams/<id>.json/', views.read_one(models.Team)),
+    path(r'teammemberships.json/', views.read_many(models.TeamMembership)),
+    path(r'teammemberships/<id>.json/', views.read_one(models.TeamMembership)),
+    path(r'comments.json/', views.read_many(models.Comment)),
+    path(r'comments/<id>.json/', views.read_one(models.Comment)),
     #path(r'contributors/<id>', views.read_many(models.Contributor)),
 ]
