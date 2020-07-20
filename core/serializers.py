b881d4395f0bdd4a56a5856d356b88551146b740
unknown <jbj1996@gmail.com>
2020-07-20 12:29:34 -0400

    replaced serializers with a more general one that displays relational fields as id #'s

--- core/serializers.py
+++ core/serializers.py
@@ -1,18 +1,37 @@
 from django.core import serializers
 from core import models
+import datetime
+
+''' original serializers:
+
 
 def json_many(model):
-    '''
-    Given a model class, retrieve a list of its objects,
-    and represent them as a JSON string.
-    '''
     data = serializers.serialize("json", model.objects.all())
     return data
 
 
 def json_one(model, id):
-    '''
-    Retrieve an object by its id and return it as a JSON string.
-    '''
     data = serializers.serialize("json", [model.objects.get(id=id)])
     return data
+
+'''
+def ModelToDict(obj):
+    result = {}
+    for field in obj._meta.fields:
+        value = getattr(obj, field.name)
+
+        #handles datetime fields
+        if type(value) == datetime.datetime:
+            value = value.isoformat()
+        #handles relational fields 
+        if hasattr(value, "id"):
+            value = value.id
+        
+        result[field.name] = value
+    return result
+
+#runs test of an object
+'''from core.models import Team
+i = Team.objects.get(id=1)
+print(ModelToDict(i))'''
+
