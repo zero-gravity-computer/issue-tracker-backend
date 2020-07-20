dadf46805099e906e0d8d35849887f5c7962eef7
unknown <jbj1996@gmail.com>
2020-07-20 13:22:20 -0400

    implemented new serializers in core/views, formatted the response

--- core/views.py
+++ core/views.py
@@ -1,23 +1,16 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from core import models, serializers
 
-
-
-'''
 def read_many(model):
     def request_handler(request):
-        data = serializers.json_one(model, id=id) pk instead of id?
-        return JsonResponse(data, safe=False)
-    return request_handler
-'''
-def read_many(model):
-    def request_handler(request):
-        data = serializers.json_many(model)
-        return JsonResponse(data, safe=False)
+        obj_list = model.objects.all()
+        data = serializers.serialize_many(obj_list)
+        return HttpResponse(data, content_type="application/json")
     return request_handler
 
 def read_one(model):
     def request_handler(request, id):
-        data = serializers.json_one(model, id=id)
-        return JsonResponse(data, safe=False)
+        obj = model.objects.get(id=id)
+        data = serializers.serialize(obj)
+        return HttpResponse(data, content_type="application/json")
     return request_handler
