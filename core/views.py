d1eecaa653c7d85a956551cf2448a23752dc4c96
unknown <jbj1996@gmail.com>
2020-07-24 18:06:07 -0400

    modified views. post to take the id of an object based on ids passed into the POST dictionary

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse, HttpResponse
 from core import models, serializers
+import json
 
 def read_many(model):
     def request_handler(request):
@@ -23,7 +24,14 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def test(request):
     if request.method =='POST':
-        return HttpResponse('WOO')
+        data = json.loads(request.body.decode("utf-8"))
+        print(data)
+        data["contributor"] = models.Contributor.objects.get(id = data["contributor"])
+        data["team"] = models.Team.objects.get(id = data["team"])
+        print(data)
+        models.TeamMembership.objects.create(**data)
+
+        return HttpResponse(str(data))
     else:
         return HttpResponse('use POST')
 
