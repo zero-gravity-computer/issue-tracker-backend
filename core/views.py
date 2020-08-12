9913154f0ec4879fa54bfe267d53b0fdd2a969a2
unknown <jbj1996@gmail.com>
2020-08-12 13:35:11 -0400

    created RUD views patterns generalized to work with all models; created error dictionary to store messages explaining misuse as a response to requests; implemented dictionary into certain views to handle expected errors.

--- core/views.py
+++ core/views.py
@@ -4,6 +4,11 @@ from core import models, serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
 
+errors = [
+    { "message": "Requested id does not exist" }
+]
+id_not_exists_err = { "message": "Requested id does not exist" }
+
 def read_many(model):
     def request_handler(request):
         obj_list = model.objects.all()
@@ -14,9 +19,12 @@ def read_many(model):
 
 def read_one(model):
     def request_handler(request, id):
-        obj = model.objects.get(id=id)
-        data = serializers.serialize(obj)
-        return HttpResponse(data, content_type="application/json")
+        try:
+            obj = model.objects.get(id=id)
+            data = serializers.serialize(obj)
+            return HttpResponse(data, content_type="application/json")
+        except:
+            return HttpResponse(str(id_not_exists_err))
     return request_handler
   
 
@@ -41,6 +49,40 @@ def create_one(model):
     return request_handler
 
 
+def update_one(model):
+    @csrf_exempt
+    def request_handler(request, id): 
+        change_data = json.loads(request.body.decode("utf-8")) 
+        try:
+            obj = model.objects.get(id=id)
+                
+            for field_name in change_data:
+                field = getattr(model, field_name)
+                if field.field.is_relation is True:
+                    related_model = field.field.related_model
+                    change_data[field_name] = related_model.objects.get(id = change_data[field_name])
+                setattr(obj, field_name, change_data[field_name])
+
+            obj.save()
+            data = serializers.serialize(obj)
+            return HttpResponse(data, content_type="application/json")
+        except:
+            return HttpResponse(str(id_not_exists_err))
+    return request_handler
+
+
+def delete_one(model):
+    @csrf_exempt
+    def request_handler(request, id):
+        try:
+            obj = model.objects.get(id=id)
+            obj.delete()
+            return HttpResponse("object deleted")
+        except:
+            return HttpResponse(str(id_not_exists_err))
+    return request_handler
+
+
 def resource(model):
     @csrf_exempt
     def request_handler(request, id=None):
@@ -51,4 +93,10 @@ def resource(model):
                 return read_one(model)(request, id)
             else:
                 return read_many(model)(request)
+        elif request.method == 'PUT':
+            if id:
+                return update_one(model)(request, id)
+        elif request.method =='DELETE':
+            if id:
+                return delete_one(model)(request, id)
     return request_handler
