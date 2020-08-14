cda5f68d1e2f9e0fa68120cc544e496d0049732f
J4mesB4xter <67648868+J4mesB4xter@users.noreply.github.com>
2020-08-14 13:25:25 -0400

    Feature/10 cru dviews (#13)
    * added a a csrf exempt POST view and url
    
    * modified views. post to take the id of an object based on ids passed into the POST dictionary
    
    * cleaned up relationship between teams, added POST view for database
    
    * generalized POST request for object creation
    
    * added resource view, cleaned up urls.py
    
    * edited view.create_one to show all new object fields after POST request is sent
    
    * adjusted urls to filter through requests more easily
    
    * created RUD views patterns generalized to work with all models; created error dictionary to store messages explaining misuse as a response to requests; implemented dictionary into certain views to handle expected errors.
    
    * added custom error messages to views, finished abstracting each view and forcing them to return json, expanded resource with new views and errors, generally cleaned up views folder

--- core/views.py
+++ core/views.py
@@ -1,16 +1,120 @@
 from django.http import JsonResponse, HttpResponse
+import core
 from core import models, serializers
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
+id_not_exists_err = {"message": "Requested id does not exist" }
+empty_body_err = {"message" : "Request body is empty"}
+no_body_err = {"message" : "Request body does not exist"}
+invalid_json_err = {"message" : "Request body is not valid json"}
+missing_id_err = {"message" : "required id not provided"}
+invalid_data_err = {"message" : "invalid data type received"}
+unsupported_method_err = {"message" : "request method not supported by url"}
 
 def read_many(model):
     def request_handler(request):
         obj_list = model.objects.all()
-        data = serializers.serialize_many(obj_list)
-        return HttpResponse(data, content_type="application/json")
+        data = list(map(serializers.model_to_dict, obj_list))
+        return JsonResponse({"data": data})
     return request_handler
+
 
 def read_one(model):
     def request_handler(request, id):
-        obj = model.objects.get(id=id)
-        data = serializers.serialize(obj)
-        return HttpResponse(data, content_type="application/json")
+        try:
+            obj = model.objects.get(id=id)
+        except:
+            return JsonResponse({"errors": [id_not_exists_err]})
+        data = serializers.model_to_dict(obj)
+        return JsonResponse({"data": data})        
     return request_handler
+  
+
+def create_one(model):
+    @csrf_exempt
+    def request_handler(request):
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+        except:
+            return JsonResponse({"errors": [invalid_json_err]})
+        try:
+            for key in data:
+                field = getattr(model, key)
+                if field.field.is_relation is True:
+                    related_model = field.field.related_model
+                    data[key] = related_model.objects.get(id = data[key])
+
+            m = model.objects.create(**data)
+            data = serializers.model_to_dict(m)
+            return JsonResponse({"data" : data})
+        except:
+            return JsonResponse({"errors": [invalid_data_err]})
+    return request_handler
+
+
+def update_one(model):
+    @csrf_exempt
+    def request_handler(request, id): 
+        try:
+            change_data = json.loads(request.body.decode("utf-8"))
+        except:
+            return JsonResponse({"errors": [invalid_json_err]})
+        try:
+            obj = model.objects.get(id=id)
+                
+            for field_name in change_data:
+                field = getattr(model, field_name)
+                if field.field.is_relation is True:
+                    related_model = field.field.related_model
+                    change_data[field_name] = related_model.objects.get(id = change_data[field_name])
+                setattr(obj, field_name, change_data[field_name])
+
+            obj.save()
+            data = serializers.model_to_dict(obj)
+            return JsonResponse({"data" : data})
+        except:
+            return JsonResponse({"errors": [id_not_exists_err]})
+    return request_handler
+
+
+def delete_one(model):
+    @csrf_exempt
+    def request_handler(request, id):
+        try:
+            obj = model.objects.get(id=id)
+            obj.delete()
+            data = serializers.model_to_dict(obj)
+            return JsonResponse({"data" : data})
+        except:
+            return JsonResponse({"errors": [id_not_exists_err]})
+    return request_handler
+
+
+def resource(model):
+    @csrf_exempt
+    def request_handler(request, id=None):
+        if request.method =='POST':
+            return create_one(model)(request)
+        elif request.method =='GET':
+            if id:
+                return read_one(model)(request, id)
+            else:
+                return read_many(model)(request)
+        elif request.method == 'PUT':
+            if id:
+                return update_one(model)(request, id)
+            else:
+                return JsonResponse({"errors": [missing_id_err]})
+        elif request.method =='DELETE':
+            if id:
+                return delete_one(model)(request, id)
+            else:
+                return JsonResponse({"errors": [missing_id_err]})
+        else:
+            return JsonResponse({"errors" : [unsupported_method_err]})
+    return request_handler
+
+def error_response(error):
+    return JsonResponse({"errors" : [error]})
