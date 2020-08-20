cef15e9dd0e7b4803970bd351944981e4fffc713
unknown <jbj1996@gmail.com>
2020-08-20 14:06:04 -0400

    did some nice cleanup and added comments for clarity

--- core/views.py
+++ core/views.py
@@ -12,21 +12,25 @@ invalid_json_err = {"message" : "Request body is not valid json"}
 missing_id_err = {"message" : "required id not provided"}
 invalid_data_err = {"message" : "invalid data type received"}
 unsupported_method_err = {"message" : "request method not supported by url"}
-invalid_filter_params = {"message": "invalid filter parameters"}
+
 
 def read_many(model):
     def request_handler(request):
-            # Use query strings to filter results
         params = { key: request.GET.get(key) for key in request.GET }
         filter_set=None
+
+        #applies smart filters from django
         for key in params:
             try:
                 filter_set = model.objects.filter(**{key : params[key]})
-
             except:
                 pass
+        
+        #gets all results if no filter is provided
         if filter_set is None:
             filter_set = model.objects.all()
+
+        #retrieves results
         data = list(map(serializers.model_to_dict, filter_set))
         return JsonResponse({"data": data})
     return request_handler
