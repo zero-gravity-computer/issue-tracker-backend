eb625eff895aa1309b293249c4f5d04280a27ead
Craig Fay <31521775+craigfay@users.noreply.github.com>
2020-09-24 13:33:56 -0500

    Minimize pagination error catching block

--- core/views.py
+++ core/views.py
@@ -13,9 +13,10 @@ id_not_exists_err = {"message": "Requested id does not exist" }
 empty_body_err = {"message" : "Request body is empty"}
 no_body_err = {"message" : "Request body does not exist"}
 invalid_json_err = {"message" : "Request body is not valid json"}
-missing_id_err = {"message" : "required id not provided"}
-invalid_data_err = {"message" : "invalid data type received"}
-unsupported_method_err = {"message" : "request method not supported by url"}
+missing_id_err = {"message" : "Required id not provided"}
+invalid_data_err = {"message" : "Invalid data type received"}
+unsupported_method_err = {"message" : "Request method not supported by url"}
+invalid_paginagion_params_err = {"message" : "Invalid pagination parameters provided"}
 
 
 
@@ -48,46 +49,48 @@ def read_many(model):
                         invalid_date_err = { "message": message }
                         return JsonResponse({"errors": [invalid_date_err]})
 
+        for key in params:
+            try:
+                queryset = model.objects.filter(**{key : params[key]})
+            except:
+                pass
+
+        if queryset is None:
+            queryset = model.objects.all()
+
+        after = params.get("after")
+
+        if params.get('first'):
+            first = int(params.get('first'))
+        else:
+            first = None
+
+        before = params.get("before")
+
+        if params.get('last'):
+            last = int(params.get('last'))
+        else:
+            last = None
+        if not first and not last:
+            first = 50
+
         try:
-            for key in params:
-                try:
-                    queryset = model.objects.filter(**{key : params[key]})
-                except:
-                    pass
-            
-            if queryset is None:
-                queryset = model.objects.all()
-
-            after = params.get("after")
-
-            if params.get('first'):
-                first = int(params.get('first'))
-            else:
-                first = None
-        
-            before = params.get("before")
-
-            if params.get('last'):
-                last = int(params.get('last'))
-            else:
-                last = None
-            if not first and not last:
-                first = 50
-
             pagination = paginate(queryset, first, last, after, before)
-            page = list(pagination['page'])
-            has_next_page = pagination['has_next_page']
-            
-            data = list(map(serializers.model_to_dict, page))
-
-            return JsonResponse({
-                'first_cursor': encode_cursor(page[0]),
-                'last_cursor': encode_cursor(page[-1]),
-                'has_next_page': has_next_page,
-                'data': data,
-            })
         except:
-            return JsonResponse({"invalid parameters" : "pagination parameters did not meet requirements"})
+            return JsonResponse({"errors": [invalid_pagination_params_err]})
+            
+        page = list(pagination['page'])
+        has_next_page = pagination['has_next_page']
+
+        data = list(map(serializers.model_to_dict, page))
+
+        return JsonResponse({
+            'first_cursor': encode_cursor(page[0]),
+            'last_cursor': encode_cursor(page[-1]),
+            'has_next_page': has_next_page,
+            'data': data,
+        })
+    
     return request_handler
 
 
