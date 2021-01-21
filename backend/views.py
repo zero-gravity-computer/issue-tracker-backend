6105e1ba75292a56d6b226ab927087707a5832a0
unknown <jbj1996@gmail.com>
2021-01-21 16:32:10 -0500

    error catching in user authentication implemented

--- backend/views.py
+++ backend/views.py
@@ -2,18 +2,30 @@ from core.models import Contributor
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.http import JsonResponse
 import jwt
 from .settings import SECRET_KEY
 
 
 @csrf_exempt
 def authenticate(request):
+    #invalid json provided
+    #invalid username
+    #password wrong
+    #wrong request method
     try:
         credentials = json.loads(request.body.decode("utf-8"))
-        c = Contributor.objects.get(username=credentials["username"])
-        if c.verify_password(credentials["password"]):
-            payload = {"contributor_id": c.id}
-            encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-            return HttpResponse(encoded_jwt)
     except:
-        return HttpResponse("authentication failed")
+        message = "expected POST request with json body"
+        return JsonResponse({'error':message}, status=400)
+    try:
+        c = Contributor.objects.get(username=credentials["username"])
+        c.verify_password(credentials["password"])
+        payload = {"contributor_id": c.id}
+        encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return JsonResponse({"data": {"token":encoded_jwt}})
+    except:
+        message = "incorrect username/password combination"
+        return JsonResponse({'error':message}, status=400)
+    
+
