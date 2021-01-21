9756e5c82bcfe9027cb332ff376d4a748efb3748
unknown <jbj1996@gmail.com>
2021-01-21 16:12:46 -0500

    payload/secret key implemented

--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,8 @@ from core.models import Contributor
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import jwt
+from .settings import SECRET_KEY
 
 
 @csrf_exempt
@@ -9,7 +11,9 @@ def authenticate(request):
     try:
         credentials = json.loads(request.body.decode("utf-8"))
         c = Contributor.objects.get(username=credentials["username"])
-        result = c.verify_password(credentials["password"])
-        return HttpResponse(result)
+        if c.verify_password(credentials["password"]):
+            payload = {"contributor_id": c.id}
+            encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            return HttpResponse(encoded_jwt)
     except:
         return HttpResponse("authentication failed")
